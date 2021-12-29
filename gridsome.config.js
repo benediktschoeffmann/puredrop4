cfa79e497e438d0977f126347c543bef21aa749a
Benedikt Schöffmann <benedikt.schoeffmann@gmail.com>
2021-12-29 15:59:20 +0100

    update

--- gridsome.config.js
+++ gridsome.config.js
@@ -5,6 +5,33 @@
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
 module.exports = {
-  siteName: 'Gridsome',
-  plugins: []
+  siteName: 'Pure Drop',
+  plugins: [
+    {
+      use: "@gridsome/source-filesystem",
+      options: {
+        path: "content/posts/*.md",
+        typeName: "Post",
+        refs: {
+          tags: {
+            typeName: 'Tag',
+            create: true,
+          }
+        }
+      }
+    },
+    {
+      use: "gridsome-plugin-netlify-cms",
+      options: {
+        publicPath: "/admin",
+        modulePath: `src/admin/index.js`
+      }
+    }
+  ],
+  transformers: {
+    remark: {
+      externalLinksTarget: "_blank",
+      externalLinksRel: ["nofollow", "noopener", "noreferrer"]
+    }
+  }
 }
