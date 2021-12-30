f9e966eff5da72f8c19b039288b6f166ce7a4355
Benedikt Schöffmann <benedikt.schoeffmann@gmail.com>
2021-12-30 09:23:15 +0100

    Add Contact page

---
+++ src/pages/Contact.vue
@@ -0,0 +1,32 @@
+<template>
+  <Layout>
+    <section class="hero is-small">
+        <div class="hero-body">
+            <p class="title">
+                Contact me
+            </p>
+            <div class="content">
+                <b-field label="Name">
+                    <b-input />
+                </b-field>
+                <b-field label="eMail">
+                    <b-input maxLength="30" />
+                </b-field>            
+                <b-field label="Your Message">
+                    <b-input maxlength="200" type="textarea"></b-input>
+                </b-field>    
+            </div>
+
+        </div>
+    </section>
+    
+  </Layout>
+</template>
+
+<script>
+export default {
+  metaInfo: {
+    title: 'Contact me'
+  }
+}
+</script>
