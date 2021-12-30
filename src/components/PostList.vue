9bcffb8779c3f620683b88104e4e2dd24456bca6
Benedikt Schöffmann <benedikt.schoeffmann@gmail.com>
2021-12-30 06:59:18 +0100

    add PostList component and add it to Index

---
+++ src/components/PostList.vue
@@ -0,0 +1,35 @@
+<template>
+    <section class="postList">
+        <article class="newsPost" 
+            v-for="(post, index) in $static.allPost.edges.node"
+            v-bind:key="post.id"
+            v-bind:index="index"
+            v-bind:title="post.title"
+        >
+            <h1 class="heading" v-html="$post.title" />
+            <p v-html="$post.content" />
+        </article>
+    </section>
+</template>
+
+<static-query>
+    query {
+        allPost (filter: {isNewsPost: {eq: true}}) {
+            edges {
+            node { 
+                id
+                title
+                content
+            }
+            }
+        }
+    }
+</static-query>
+
+<script>
+
+</script>
+
+<style>
+
+</style>
